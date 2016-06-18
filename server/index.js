776b4bb9b04ea0334875328dc848590440ec5327
Francis Wang <go717franciswang@gmail.com>
2016-06-18 14:48:22 -0700

    Restructure directory

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
-///<reference path='node.d.ts'/>
-///<reference path='express.d.ts'/>
-///<reference path='socket.io.d.ts'/>
+///<reference path='typings/index.d.ts'/>
+"use strict";
+var http = require("http");
+var express = require("express");
+var socketio = require("socket.io");
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
 //# sourceMappingURL=index.js.map
