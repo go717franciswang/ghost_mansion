d6ab72a7c37364f20c90795b0eae531e3b62604a
Francis Wang <go717franciswang@gmail.com>
2016-06-18 18:40:57 -0700

    Test sending commands

--- server/index.js
+++ server/index.js
@@ -1,22 +1,35 @@
 ///<reference path='typings/index.d.ts'/>
 "use strict";
 var socketio = require("socket.io");
-var Promise = require("promise");
-var fs = require("fs");
-var loadMap = function (mapName) {
-    return new Promise(function () {
-        fs.readFile(__dirname + '/../resources/ghost_mansion/' + mapName + '.json', function (err, data) {
-            if (err)
-                throw err;
-            var map = JSON.parse(data.toString());
-            var walls = map.layers.filter(function (l) { return l.name == 'walls'; })[0];
-            Promise.resolve(walls);
-        });
-    });
-};
+var maps = {};
+var mapNames = ["main-floor", "research-lab"];
+mapNames.forEach(function (m) {
+    maps[m] = require(__dirname + '/../resources/ghost_mansion/' + m + '.json');
+});
 var io = socketio();
+var playerId = 0;
 io.on('connection', function (socket) {
     console.log('a user connected');
+    console.log(maps);
+    var player = {
+        position: { x: 50, y: 50 },
+        flashlight: 100,
+        playerId: playerId++
+    };
+    socket.on('move', function (data) {
+        console.log('got move command');
+        player.position.x += data.dx;
+        player.position.y += data.dy;
+    });
+    socket.on('flashlight', function (data) {
+        console.log('got flashlight command');
+        if (data.on) {
+            player.flashlight -= 1;
+        }
+    });
+    setInterval(function () {
+        console.log(player);
+    }, 1000);
 });
 io.listen(3000);
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,41 @@
 ///<reference path='typings/index.d.ts'/>
 
 import socketio = require("socket.io");
-import Promise = require("promise");
 import fs = require("fs");
 
-var loadMap = (mapName) => {
-    return new Promise(() => {
-        fs.readFile(__dirname + '/../resources/ghost_mansion/'+mapName+'.json', (err, data) => {
-            if (err) throw err;
-            var map = JSON.parse(data.toString());
-            var walls = map.layers.filter((l) => { return l.name == 'walls' })[0];
-            Promise.resolve(walls);
-        });
-    });
-};
+var maps = {};
+var mapNames = ["main-floor", "research-lab"];
+mapNames.forEach((m) => { 
+    maps[m] = require(__dirname + '/../resources/ghost_mansion/'+m+'.json');
+});
 
 var io = socketio();
+var playerId = 0;
 io.on('connection', (socket) => {
     console.log('a user connected');
+    console.log(maps);
+    var player = {
+        position: { x: 50, y: 50 },
+        flashlight: 100,
+        playerId: playerId++
+    };
+
+    socket.on('move', (data) => {
+        console.log('got move command');
+        player.position.x += data.dx;
+        player.position.y += data.dy;
+    });
+
+    socket.on('flashlight', (data) => {
+        console.log('got flashlight command');
+        if (data.on) {
+            player.flashlight -= 1;
+        }
+    });
+
+    setInterval(() => {
+        console.log(player);
+    }, 1000);
 });
 
 io.listen(3000);
